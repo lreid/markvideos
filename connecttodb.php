be68b84dc8442a1a49d83eb4e342327376cec85e
laura <lreid2@uwo.ca>
2018-10-10 18:41:28 +0000

    push files from my vm to github

---
+++ connecttodb.php
@@ -0,0 +1,47 @@
+<?php
+$connection="";
+
+function openDatabase() {
+	global $connection;
+
+	$dbhost = "localhost";
+	$dbuser= "root";
+	$dbpass = "cs3319";
+	$dbname = "markingvideo";
+	$connection = mysqli_connect($dbhost, $dbuser,$dbpass,$dbname);
+	if (mysqli_connect_errno()) {
+	     die("database connection failed :" .
+	     mysqli_connect_error() .
+	     "(" . mysqli_connect_errno() . ")"
+	         );
+	    }	
+}
+
+function getTheStudent() {
+	
+}
+
+
+//load up the marking group of videos
+function loadBigGroups() {
+        global $connection;
+	$query = "Select biggroupid from biggroup";
+	$result=mysqli_query($connection,$query);
+	if (!$result) {
+		 die ("Database query failed");
+	}
+	while ($row = mysqli_fetch_assoc($result)) {
+		echo '<option value="' . $row['biggroupid']. '">Group '. $row["biggroupid"].'</option>';
+	}
+	mysqli_free_result($result);
+}
+
+function getStudentInfo() {
+
+
+}
+
+openDatabase();
+
+
+?>
